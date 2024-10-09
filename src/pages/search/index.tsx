beafc4ae7db6b7802faddb261450271827468bc0
Liraz Zeidman <liraz.idf@gmail.com>
2024-10-09 15:11:39 +0300

    fix transition by figma demo

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,23 +1,137 @@
-import { Grid } from '@mui/material';
+import { useTheme, Grid, Fade, styled, Box, keyframes } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import HistoryResults from './pages/historyResults';
-import SearchResults from './pages/SearchResults';
+import Results from './components/results';
+import EmptyResults from '../../assets/icons/emptyResults.svg';
+import { useEffect, useRef, useState } from 'react';
+import { ResultsTypes } from '../../lib/enums';
+import { useDebounce } from '@uidotdev/usehooks';
+import { useQuery } from '@tanstack/react-query';
+import { getCountsBySearchTermRequest, searchRequest } from '../../services/searchService';
+import { ResultsMenu } from './components/resultsMenu';
+const env = import.meta.env;
+
+const growRightToLeft = keyframes`
+  from {
+    width: 0;
+    transform-origin: right;
+  }
+  to {
+    width: 100%;
+    transform-origin: right;
+  }
+`;
+
+const SlideBox = styled(Box)({
+  width: '100%',
+  animation: `${growRightToLeft} 0.6s ease forwards`,
+  transformOrigin: 'right',
+});
+
+const FadeBox = styled(Box)({
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+});
 
 const Search = () => {
+  const theme = useTheme();
+
+  const [page, setPage] = useState(1);
+  const [resultsType, setResultsType] = useState<ResultsTypes>(ResultsTypes.ENTITY);
+  const scrolledElementRef = useRef<HTMLDivElement | null>(null);
+
   const searchTerm = useSelector((state: RootState) => state.search.searchTerm);
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  const { data: counts } = useQuery({
+    queryKey: ['getCountsBySearchTermRequest', debouncedSearchTerm],
+    queryFn: () => getCountsBySearchTermRequest(debouncedSearchTerm),
+    initialData: {
+      entity: 0,
+      goalUser: 0,
+      group: 0,
+    },
+    enabled: !!debouncedSearchTerm,
+  });
+
+  const { data: searchResults } = useQuery({
+    queryKey: ['search', debouncedSearchTerm, resultsType, page],
+    queryFn: () => searchRequest(debouncedSearchTerm, resultsType, page, +env.VITE_BACKEND_PAGE_SIZE),
+    initialData: [],
+    enabled: !!debouncedSearchTerm,
+  });
+
+  useEffect(() => {
+    setPage(1);
+    // scrolledElementRef.current!.scrollTop = 0;
+  }, [searchTerm, resultsType]);
 
   return (
     <Grid
       container
       width={'74.7%'}
-      flexDirection={'column'}
       alignContent={'center'}
       alignSelf={'center'}
-      mt={3}
+      mt={2}
       position="relative"
+      justifyContent={'center'}
     >
-      {searchTerm.length < 2 ? <HistoryResults /> : <SearchResults />}
+      <Grid container>
+        <Grid
+          item
+          xs={searchResults?.length !== 0 ? 2.5 : 0}
+          sx={{
+            overflow: 'hidden',
+            opacity: searchResults?.length !== 0 ? 1 : 0,
+            transform: searchResults?.length !== 0 ? 'translateX(0)' : 'translateX(-20px)',
+            display: searchResults?.length === 0 ? 'none' : 'unset',
+            transition: 'opacity 50s ease, transform 50s ease',
+            minWidth: '200px',
+          }}
+        >
+          <SlideBox>
+            <ResultsMenu resultsType={resultsType} setResultsType={setResultsType} counts={counts} />
+          </SlideBox>
+        </Grid>
+
+        <Grid item>
+          {searchTerm.length < 2 && !searchResults.length ? (
+            <HistoryResults />
+          ) : (
+            <>
+              <Fade in={searchResults?.length === 0} timeout={500}>
+                <FadeBox sx={{ display: searchResults?.length === 0 ? 'block' : 'none', textAlign: 'center' }}>
+                  <img
+                    src={EmptyResults}
+                    style={{
+                      background: '#F7F7F7',
+                      borderRadius: 12,
+                      marginBottom: 15,
+                      width: '85%',
+                    }}
+                  />
+                </FadeBox>
+              </Fade>
+
+              <Fade in={searchResults?.length !== 0} timeout={500}>
+                <FadeBox sx={{ display: searchResults?.length !== 0 ? 'block' : 'none', width: '94.5%' }}>
+                  <Grid item xs={10} height={'79vh'} sx={{ justifyContent: 'center' }}>
+                    <Results
+                      type={resultsType}
+                      results={searchResults}
+                      count={counts?.[resultsType] ?? 0}
+                      setPage={setPage}
+                      scrolledElementRef={scrolledElementRef}
+                    />
+                  </Grid>
+                </FadeBox>
+              </Fade>
+            </>
+          )}
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
