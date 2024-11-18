d0c48e08edce0a3fd86b26bf41576df8fe5f122f
tommyafek <tommyafek@gmail.com>
2024-11-18 12:06:45 +0200

    fix: folder uppercases bug

---
+++ src/layout/heroSection/index.tsx
@@ -0,0 +1,72 @@
+import heroBackground from '../../assets/images/heroBackground.svg';
+import heroLayer from '../../assets/images/heroLayer.svg';
+import { Box } from '@mui/material';
+import { SearchBar } from '../../common/searchBar';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+export const HeroSection = () => {
+  const location = useLocation();
+
+  const [isHome, setIsHome] = useState(location.pathname === '/');
+
+  useEffect(() => {
+    setIsHome(location.pathname === '/');
+  }, [location]);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: `${isHome ? '45%' : '8%'}`,
+        width: '85%',
+        margin: '1rem 0rem 2rem',
+        position: 'relative',
+        alignSelf: 'center',
+        transition: 'height 0.7s',
+      }}
+    >
+      <>
+        <img
+          src={heroBackground}
+          style={{
+            width: '100%',
+            objectFit: 'cover',
+            objectPosition: `${isHome ? 'bottom' : 'top'}`,
+            borderRadius: `${isHome ? '2rem' : '1rem'}`,
+            transition: 'border-radius 0.7s, filter 0.4s',
+            filter: isHome ? '' : 'brightness(0.9) saturate(2) contrast(1.1)',
+          }}
+        />
+        <img
+          src={heroLayer}
+          style={{
+            width: '100%',
+            position: 'absolute',
+            objectFit: 'cover',
+            height: '100%',
+            overflow: 'hidden',
+            objectPosition: 'top',
+            borderRadius: `${isHome ? '2rem' : '1rem'}`,
+            opacity: isHome ? 1 : 0,
+            transition: 'opacity 0.4s', // Smooth transition for the opacity
+          }}
+        />
+      </>
+
+      <Box
+        sx={{
+          position: 'absolute',
+          bottom: `${isHome ? '-9%' : '-30%'}`,
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          transition: 'bottom 0.7s',
+        }}
+      >
+        <SearchBar />
+      </Box>
+    </Box>
+  );
+};
---
+++ src/layout/topbar/index.tsx
@@ -0,0 +1,71 @@
+import { Box, Divider, useTheme, IconButton } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store';
+import { ContactDrawer } from '../../common/drawer/drawerWrapper';
+import { useNavigate } from 'react-router-dom';
+import Yesodot from '../../assets/icons/yesodot.svg';
+import Sapir from '../../assets/icons/sapir.svg';
+import icon from '../../assets/icons/icon.svg';
+import { ProfileImage } from '../../common/profileImage';
+import { setSearchTerm } from '../../store/reducers/search';
+import { setDrawerObject, setIsDrawerOpen } from '../../store/reducers/drawer';
+
+const TopBar = () => {
+  const currentUser = useSelector((state: RootState) => state.user);
+  const theme = useTheme();
+  const contact = useSelector((state: RootState) => state.drawer.contact);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        color: theme.colors.white,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <IconButton
+        onClick={() => {
+          navigate('/');
+          dispatch(setSearchTerm(''));
+        }}
+        sx={{ p: 0, '&:hover': { background: 'none' } }}
+      >
+        <img src={icon} style={{ width: '5rem', alignSelf: 'center' }} />
+      </IconButton>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          columnGap: theme.spacing(3),
+        }}
+      >
+        <Box sx={{ display: 'flex', flexDirection: 'row', columnGap: theme.spacing(1.8) }}>
+          <img src={Sapir} style={{ height: '2rem', alignSelf: 'center' }} />
+          <img src={Yesodot} style={{ height: '2rem', alignSelf: 'center' }} />
+        </Box>
+
+        <Divider orientation="vertical" flexItem sx={{ height: '2.5rem' }} />
+
+        <ProfileImage
+          type={currentUser.entityType === 'GoalUser' ? 'goalUser' : 'entity'}
+          id={currentUser.id}
+          style={{ width: '2.5rem', height: '2.5rem', cursor: 'pointer' }}
+          onClick={() => {
+            dispatch(setIsDrawerOpen(true));
+            dispatch(setDrawerObject(currentUser));
+          }}
+          sex={currentUser.sex}
+        />
+      </Box>
+      <ContactDrawer contact={contact} />
+    </Box>
+  );
+};
+
+export default TopBar;
