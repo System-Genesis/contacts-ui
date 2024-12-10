a564bfca0e0085e06968d37708ba43d4635a8ae9
amitpollak1 <amitpollak05@gmail.com>
2024-12-10 15:48:41 +0200

    feat: chatbot

--- src/layout/chatBot/index.tsx
+++ src/layout/chatBot/index.tsx
@@ -1,8 +1,15 @@
 import { Fab, useTheme } from '@mui/material';
 import chatBotSrc from '../../assets/icons/ChatBot.svg';
+import { useState } from 'react';
+import { ChatBotDialog } from '../../common/dialogs/chatBot';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 export default function ChatBot() {
   const theme = useTheme();
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const config = useSelector((state: RootState) => state.config);
 
   return (
     <Fab
@@ -16,10 +23,26 @@ export default function ChatBot() {
         ':hover': {
           background: theme.colors.aquaDark,
         },
-        display: 'none',
       }}
     >
-      <img src={chatBotSrc} alt="Chatbot Icon" />
+      <img
+        src={chatBotSrc}
+        alt="Chatbot Icon"
+        onClick={() => {
+          setDialogOpen(true);
+        }}
+      />
+      <ChatBotDialog
+        open={dialogOpen}
+        setOpen={setDialogOpen}
+        onCancel={() => {
+          setDialogOpen(false);
+        }}
+        onSave={() => {
+          setDialogOpen(false);
+          window.open(`${config.serviceNowUrl}`);
+        }}
+      />
     </Fab>
   );
 }
