38dda4340d2da29e0b073ea71e7481fedfdf4a23
Itay Marder <marderit@gmail.com>
2025-04-06 17:53:32 +0300

    wip

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import envConfig from './config/env.config';
-import { ListenerModule } from './listener/listener.module';
-
-@Module({
-  imports: [MongooseModule.forRoot(envConfig.mongo.uri), ListenerModule],
-})
-export class AppModule {}
